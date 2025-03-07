76d6a90e702086835dc7d98c0f2cd84a1731ffa7
angel <angelicaizabella90@gmail.com>
2025-03-07 10:13:00 +0700

    home screen update

--- film/lib/screens/home_screen.dart
+++ film/lib/screens/home_screen.dart
@@ -1,35 +1,38 @@
 import 'package:film/models/movie.dart';
 import 'package:film/services/api_services.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
-  @override
+  @override 
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  //variabel mewakili api services
   final ApiService _apiService = ApiService();
 
   List<Movie> _allMovies = [];
   List<Movie> _trendingMovies = [];
   List<Movie> _popularMovies = [];
+  bool _isLoading = true;
 
   Future<void> _loadMovies() async {
-    final List<Map<String, dynamic>> allMoviesdata =
-        await _apiService.getAllMovies();
-    final List<Map<String, dynamic>> trendingMoviesdata =
-        await _apiService.getTrendingMovies();
-    final List<Map<String, dynamic>> popularMoviesdata =
-        await _apiService.getPopularMovies();
-    setState(() {
-      _allMovies = allMoviesdata.map((e) => Movie.fromJson(e)).toList();
-      _trendingMovies =
-          trendingMoviesdata.map((e) => Movie.fromJson(e)).toList();
-      _popularMovies = popularMoviesdata.map((e) => Movie.fromJson(e)).toList();
-    });
+    try {
+      final List<Map<String, dynamic>> _allMoviesData = await _apiService.getAllMovies();
+      final List<Map<String, dynamic>> _trendingMoviesData = await _apiService.getTrendingMovies();
+      final List<Map<String, dynamic>> _popularMoviesData = await _apiService.getPopularMovies();
+    
+      setState(() {
+        _allMovies = _allMoviesData.map((e) => Movie.fromJson(e)).toList();
+        _trendingMovies = _trendingMoviesData.map((e) => Movie.fromJson(e)).toList();
+        _popularMovies = _popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+        _isLoading = false;
+      });
+    } catch (e) {
+      print("Error loading movies: $e");
+    }
   }
 
   @override
@@ -37,25 +40,25 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     _loadMovies();
   }
-}
 
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pilem'),
+        title: const Text('Film'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildMovieList('All Movies', _allMovies),
-            _buildMovieList('Trending Movies', _trendingMovies),
-            _buildMovieList('Popular Movies', _popularMovies),
-          ],
-        ),
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildMovieList('All Movies', _allMovies),
+                  _buildMovieList('Trending Movies', _trendingMovies),
+                  _buildMovieList('Popular Movies', _popularMovies),
+                ],
+              ),
+            ),
     );
   }
 
@@ -70,7 +73,6 @@ class _HomeScreenState extends State<HomeScreen> {
             style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
-        // Movie List
         SizedBox(
           height: 200,
           child: ListView.builder(
@@ -84,12 +86,19 @@ class _HomeScreenState extends State<HomeScreen> {
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
                     children: [
-                      Image.network(
-                        'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                        height: 150,
-                        width: 100,
-                        fit: BoxFit.cover,
-                      ),
+                      movie.posterPath.isNotEmpty
+                          ? Image.network(
+                              'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                              height: 150,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            )
+                          : Container(
+                              height: 150,
+                              width: 100,
+                              color: Colors.grey,
+                              child: const Icon(Icons.image_not_supported),
+                            ),
                       const SizedBox(height: 5),
                       Text(
                         movie.title.length > 14
@@ -103,53 +112,8 @@ class _HomeScreenState extends State<HomeScreen> {
               );
             },
           ),
-        ),
+        )
       ],
     );
   }
-
-
-  Widget _buildMovieList(String title, List<Movie> movies) {
-
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          title,
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
-
-      // Movie List
-      SizedBox(
-        height: 200,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: movies.length,
-          itemBuilder: (context, index) {
-            final Movie movie = movies[index];
-            return GestureDetector(
-              onTap: () {},
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Image.network('https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                    height: 150,
-                    width: 100,
-                    fit: BoxFit.cover,
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      movie.title.length > 14 ?
-                      '${movie.title.substring(0,10)}...'
-                      : movie.title,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
+}
